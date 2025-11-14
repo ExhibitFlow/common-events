e0a1e4261510c3cb49d0f6fa7c010419c2cc2aee
Tharindu Jayawardhana <tharinduimalkajayawardhana@gmail.com>
2025-11-14 17:41:59 +0530

    feat:initialized the project package

---
+++ src/main/java/com/exhibitflow/common_events/App.java
@@ -0,0 +1,10 @@
+package com.exhibitflow.common_events;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
